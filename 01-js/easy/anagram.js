2cc4e66e6a3967db43b3f3531283fdd259ee647b
Akshay Dinesh <akshay5632@gmail.com>
2023-12-07 20:44:32 +0530

    isAnagram complete

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,22 @@
 */
 
 function isAnagram(str1, str2) {
+  // Converting to lower case first and then into an array
+  const str1Arr = str1.toLowerCase().split("");
+  const str2Arr = str2.toLowerCase().split("");
 
+  // Comparing array lengths
+  if (str1Arr.length != str2Arr.length) return false;
+
+  // Sorting both the array
+  const sortedOne = str1Arr.sort();
+  const sortedTwo = str2Arr.sort();
+
+  // Comparing letter by letter of both arrays
+  for (let i = 0; i < sortedOne.length; i++) {
+    if (sortedOne[i] != sortedTwo[i]) return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
